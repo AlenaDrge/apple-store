c47cc5dfb4a8ec1e0d2421542eb24fde3d7c21da
AlenaDrge <phudao1712@gmail.com>
2026-03-09 16:38:27 +0700

    first commit

--- js/account.js
+++ js/account.js
@@ -2,7 +2,7 @@ const sampleUsers = [
     {
         id: 1,
         name: 'Admin',
-        email: 'admin@example.com',
+        email: 'admin@gmail.com',
         password: 'admin123',
         phone: '0900000001',
         address: '123 Đường Apple, Quận 1, TP.HCM',
@@ -12,7 +12,7 @@ const sampleUsers = [
     {
         id: 2,
         name: 'User 01',
-        email: 'user@example.com',
+        email: 'user@gmail.com',
         password: 'user123',
         phone: '0900000002',
         address: '35 Nguyễn Tri Phương, TP.HCM',
@@ -22,7 +22,7 @@ const sampleUsers = [
     {
         id: 3,
         name: 'Shipper 01',
-        email: 'shipper@example.com',
+        email: 'shipper@gmail.com',
         password: 'shipper123',
         phone: '0900000003',
         address: '25 Lý Thường Kiệt, TP.HCM',
@@ -36,6 +36,16 @@ const sampleUsers = [
         password: 'phu123',
         phone: '0123456789',
         address: '85 Nguyễn Thượng Hiền, TP.HCM',
+        isAdmin: false,
+        role: 'user'
+    },
+    {
+        id: 5,
+        name: 'Vĩnh Phát',
+        email: 'phat@gmail.com',
+        password: 'phat123',
+        phone: '0123456788',
+        address: '25 Phan Đăng Lưu, TP.HCM',
         isAdmin: false,
         role: 'user'
     }
